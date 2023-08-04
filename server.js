d4df2b19a2fd82cf0f7f5aa0ad2d94a7eb5d85d1
Aniruddh <adubge@gmail.com>
2023-08-04 21:00:14 +0530

    chat application is kinda gae

---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+
+    socket.emit('message', 'Welcome to Chatbored Dumbass!~');
+
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has left the chat')
+    });
+
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    })
+});
+
+const PORT = 3000 || process.env.PORT ;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
