680fd399512143f2230240b85f3bcc245dcc8f06
Aniruddh <adubge@gmail.com>
2023-08-04 23:28:06 +0530

    chatbored is working

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
+
 
 const app = express();
 const server = http.createServer(app);
@@ -9,19 +12,44 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatBored Maid';
+
 io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room}) => {
+        const user = userJoin(socket.id, username, room);
+        
+        socket.join(user.room);
 
-    socket.emit('message', 'Welcome to Chatbored Dumbass!~');
+        socket.emit('message', formatMessage(botName, 'Welcome to Chatbored Dumbass!~'));
 
-    socket.broadcast.emit('message', 'A user has joined the chat');
-
-    socket.on('disconnect', () => {
-        io.emit('message', 'User has left the chat')
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));   
+        
+        io.to(user.room).emit('roomUsers', {
+            room: user.room, 
+            users: getRoomUsers(user.room)
+        });
+    
     });
 
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
     })
+
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName,`${user.username} has left the chat`));
+            
+            io.to(user.room).emit('roomUsers', {
+                room: user.room, 
+                users: getRoomUsers(user.room)
+            });
+        }    
+    });
+
 });
 
 const PORT = 3000 || process.env.PORT ;
